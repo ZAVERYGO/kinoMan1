27a500b36c1cf58fe12e692d547dd3d0084d9a45
ZAVERYGO <Hockey.nick932@gmail.com>
2024-01-08 02:33:05 +0300

    2

---
+++ src/main/java/com/kozich/KinoMan/model/dto/CatalogDto.java
@@ -0,0 +1,32 @@
+package com.kozich.KinoMan.model.dto;
+
+import com.kozich.KinoMan.model.entity.ActorEntity;
+import com.kozich.KinoMan.model.entity.GenreEntity;
+import jakarta.persistence.Column;
+import lombok.Data;
+
+import java.sql.Time;
+import java.util.List;
+
+@Data
+public class CatalogDto {
+    private Long id;
+
+    private String type;
+
+    private String name;
+
+    private String director;
+
+    private String country;
+
+    private Time duration;
+
+    private Integer released;
+
+    private String description;
+
+    private List<ActorDto> actors;
+
+    private List<GenreDto> genres;
+}
