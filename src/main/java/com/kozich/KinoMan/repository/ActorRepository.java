bd512eaa86b44829f2d0ae52a9275390d8dc7151
ZAVERYGO <Hockey.nick932@gmail.com>
2024-01-25 16:12:21 +0300

    2

--- src/main/java/com/kozich/KinoMan/repository/ActorRepository.java
+++ src/main/java/com/kozich/KinoMan/repository/ActorRepository.java
@@ -11,13 +11,6 @@ import java.util.List;
 @Repository
 public interface ActorRepository extends JpaRepository<ActorEntity, Long> {
     ActorEntity findByFirstNameAndLastName(String firstName, String lastName);
-
-    //нерабочие
-    //@Query(value = "SELECT first_Name from actor where actor.actor_id = (SELECT actor_id from catalog_actor WHERE catalog_actor.catalog_id = (SELECT name from catalog WHERE name =:name))", nativeQuery = true)
-    //@Query("select e from CatalogActorEntity e where e.catalogId.name = :name")
-    //@Query("select a from (select c, a from CatalogActorEntity c, ActorEntity a WHERE c.actorId.id = a.id) where c.catalogId.name = :name")
-
-    //рабочие
     //@Query("select a from CatalogActorEntity c join c.actorId a on c.catalogId.name =:name")
     @Query("select c.actorId from CatalogActorEntity c where c.catalogId.name=:name")
     List<ActorEntity> findByMovie(@Param("name") String name);
